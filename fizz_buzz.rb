8bdab90e66c80d1a4f8c2718f0c7e7d0588c4b0b
19951118 <reonn1118@yahoo.co.jp>
2023-12-19 05:21:49 +0000

    fizzbuzz2

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,16 +1,16 @@
-def fizz_buzz(num)
-  if num % 15 == 0
+def fizzbuzz(num)
+  if num % 5 == 0 && num % 3 == 0
     "fizzbuzz"
   elsif num % 5 == 0
     "buzz"
   elsif num % 3 == 0
     "fizz"
-  else 
+  else
     num.to_s
   end
 end
 
-puts "数字を入力してください。"
+puts "数字を入れてね"
 num = gets.to_i
 puts "結果↓"
-puts fizz_buzz(num)
+puts fizzbuzz(num)
